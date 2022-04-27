270778697b931e25217b8f9324c19aea00a1814f
Arif6474 <arifulislam64743@gmail.com>
2022-04-28 02:43:51 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config()
 
@@ -8,7 +9,22 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-
+function verifyJWT(req, res, next) {
+    const authHeaders = req.headers.authorization;
+    if(!authHeaders){
+        return res.status(401).send({message: 'Unauthorized access'})
+    }
+    const token = authHeaders.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(401).send({message: 'forbidden access'})
+        }
+        console.log('decoded', decoded);
+        req.decoded=decoded;
+        next();
+    })
+        
+}
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.baljp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,7 +33,18 @@ async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db("geniusCar").collection("services")
-        
+        const orderCollection = client.db("geniusCar").collection("order")
+
+        //post
+        app.post('/login', async (req, res) =>{
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+                expiresIn : '1d'
+            })
+            res.send({accessToken});
+        })
+
+        // get service
         app.get('/service', async (req, res) =>{
         const query ={}
         const cursor = serviceCollection.find(query);
@@ -32,13 +59,12 @@ async function run(){
             res.send(service);
         })
         //post
-        app.post('/service', async (req, res) =>{
+         app.post('/service', async (req, res) =>{
             const newService = req.body;
             const result= await serviceCollection.insertOne(newService);
             res.send(result);
-
-
-        })
+         })
+         
     //delete
         app.delete('/service/:id', async (req, res) =>{
             const id = req.params.id;
@@ -46,6 +72,25 @@ async function run(){
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
         })
+
+    // order post 
+    app.post('/order', async(req, res) => {
+
+        const order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.send(result);
+    })
+    // order get
+    app.get('/order' ,verifyJWT, async(req, res) => {
+        const decodedEmail = req.decoded.email
+        const email = req.query.email;  
+       if (email === decodedEmail) {
+        const query = {email: email};
+        const cursor =  orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+       }
+    })
     }
     finally{
 
